261541b481f5f5b3e157a20e674476107f093bdf
Tsvetan <tvetan@gbg.bg>
2019-02-13 23:49:41 +0200

    Added initial program for getting the times from sofiatraffic schedules

---
+++ tools/WebScraperHelpers/SofiaMetroSchedulesTrafficScraper/Program.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SofiaMetroSchedulesTrafficScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://schedules.sofiatraffic.bg/metro/1");
+            var pageContents = await response.Content.ReadAsStringAsync();
+
+            var pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            var times = pageDocument.DocumentNode.SelectNodes("(//div[@class='schedule_view_direction']//div[@class='schedule_times']//tbody//a)");
+            foreach (var time in times)
+            {
+                Console.WriteLine(time.InnerText);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
